043c0bf49260fabe0278dd03dbecc008a3191550
Malik Kilgore <malik199228@gmail.com>
2020-01-18 04:36:19 -0500

    Fixed Calc layout/spacing. Display now updates with each number button clicked.

--- calculator.js
+++ calculator.js
@@ -1,22 +1,22 @@
+const liveDisplay = document.querySelector('.display')
+
 function add(x, y) {
-    return x + y
-}
+    return x + y;
+};
 
 function subtract(x, y) {
-    return x - y
-}
+    return x - y;
+};
 
 function multiply(x, y) {
-    return x * y
-}
+    return x * y;
+};
 
 function divide(x, y) {
-    return x / y
-}
-
+    return x / y;
+};
 
 function operate(x, y) {
-
 
     switch(input){
         case '+':
@@ -27,6 +27,40 @@ function operate(x, y) {
             return multiply(x,y)
         case '/':
             return divide(x, y)
+    };
 
-    }
-}
+};
+
+function receiveInput(buttonClick) {
+
+    switch(buttonClick) {
+        case '0':
+            return liveDisplay.innerHTML += '0'
+        case '1':
+            return liveDisplay.innerHTML += '1'
+        case '2':
+            return liveDisplay.innerHTML += '2'
+        case '3':
+            return liveDisplay.innerHTML += '3'
+        case '4':
+            return liveDisplay.innerHTML += '4'
+        case '5':
+            return liveDisplay.innerHTML += '5'
+        case '6':
+            return liveDisplay.innerHTML += '6'
+        case '7':
+            return liveDisplay.innerHTML += '7'
+        case '8':
+            return liveDisplay.innerHTML += '8'
+        case '9':
+            return liveDisplay.innerHTML += '9'
+    };
+};
+
+let numberButtons = Array.from(document.getElementsByClassName('numbers'))
+for (let i = 0; i < numberButtons.length; i++) {
+    numberButtons[i].addEventListener('click', function(e) {
+        let buttonClick = this.id
+        receiveInput(buttonClick)
+    });
+};
